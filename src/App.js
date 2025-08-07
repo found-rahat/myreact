9a5c35f82f3cb92392941419d3cabd3b81ee90bd
found-rahat <206825051+irahat120@users.noreply.github.com>
2025-08-07 12:52:28 +0600

    install react

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Khan } from "./components/greet";
+import Welcome from "./components/classcomponents";
+import Massage from "./components/massage";
+import Counter from "./components/counter";
+import FunctionClick from "./components/click";
+import Eventbind from "./components/eventbind";
+
+function App() {
+  return (
+    <div className="App">
+      <Eventbind name="hello sir My name id rahat" />
+      {/* <FunctionClick /> */}
+      {/* <Counter counts={10} /> */}
+      {/* <Massage /> */}
+      {/* <Khan name="Ridoy" done="spiderman" />
+      <Khan name="Ratul" done="batman" /> */}
+      {/* <Welcome /> */}
+    </div>
+  );
+}
+
+export default App;
